ea447eaf4f1f7e502db279d9103bbb965f6b9c82
Justin Patrick <jpatrick6420@gmail.com>
2023-12-15 20:44:20 -0700

    still..

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "jPatrick6420.github.io/woodCreek/",
+  base: "/woodCreek/",
   plugins: [react()],
 });
